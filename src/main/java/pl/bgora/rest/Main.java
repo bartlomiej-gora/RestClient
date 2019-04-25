c60bda2a10ba119b8d711ae664b86d5a6dde1208
bartlomiej-gora <bartlomiej.gora@gmail.com>
2019-04-25 20:10:59 +0200

    test

--- src/main/java/pl/bgora/rest/Main.java
+++ src/main/java/pl/bgora/rest/Main.java
@@ -2,13 +2,35 @@ package pl.bgora.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @ImportResource("hello-integration.xml")
 public class Main {
 
+
+    private HelloGateway helloGateway;
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        ApplicationContext app =  SpringApplication.run(Main.class, args);
+        final Main main = new Main();
+        main.setHelloGateway((HelloGateway) app.getBean("helloGateway"));
+        main.sayHello();
+    }
+
+
+    public void sayHello(){
+        final SayHelloRequest sayHelloRequest = new SayHelloRequest();
+        sayHelloRequest.setName("Bartek");
+        sayHelloRequest.setSurname("Góra");
+        final SayHelloResponse response = helloGateway.sayHello(sayHelloRequest);
+        System.out.println("Response: " + response.getMessage());
+    }
+
+    public void setHelloGateway(final HelloGateway helloGateway) {
+        this.helloGateway = helloGateway;
     }
 }
